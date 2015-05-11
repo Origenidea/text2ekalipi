a01f335b1155cbfba5b14d261c6e5871a0ff532f
Chris McKenzie <kristopolous@yahoo.com>
2015-05-11 13:17:32 -0700

    refactoring

--- text2ekalip.py
+++ text2ekalip.py
@@ -40,6 +40,43 @@ def word2cmu(word):
     else:
         return word
 
+def word2ek(word):
+    cmu = word2cmu(word)
+    # This means that the CMU dict didn't
+    # have the word to begin with
+    if type(cmu) is str:
+        return "MISSING"
+
+    # This always returns a list
+    ek = cmu2ek(cmu)
+    return ''.join(ek)
+
+def transline(line):
+    # remove numerics.
+    line = re.sub(r'[\d_]+', '', line)
+
+    wordlist = re.split(r'[^\w]+', line)
+
+    # remove the empty strings
+    wordlist = filter(None, wordlist)
+
+    # If this results in a blank line,
+    # then we just loop again
+    if len(wordlist) == 0:
+        continue
+
+    for word in wordlist:
+        cmu = word2cmu(word)
+
+        # This means that we couldn't find
+        # this word in the CMU dict.
+        if type(cmu) is str:
+            print word + " " + str(cmu)
+
+        else:
+            ek = cmu2ek(cmu)
+            print ''.join(ek)
+
 loadcmu()
 loadek()
 
@@ -59,6 +96,7 @@ while 1:
     if len(wordlist) == 0:
         continue
 
+    word2cmu(
     for word in wordlist:
         cmu = word2cmu(word)
 
