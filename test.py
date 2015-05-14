e5461138ce382a2bfa37310e65f346e8fc7eda2f
Chris McKenzie <kristopolous@yahoo.com>
2015-05-14 15:05:18 -0700

    more parsing

--- test.py
+++ test.py
@@ -6,7 +6,14 @@ prefix = '{http://www.mediawiki.org/xml/export-0.10/}'
 for event, elem in etree.iterparse('./ref/enwiktionary.xml', events=('start', 'end', 'start-ns', 'end-ns')):
     if event == 'start':
         if elem.tag == prefix + 'title':
-            print str(elem), elem.text
+            title = elem.text
+        elif elem.tag == prefix + 'ns' and elem.text == '0':
+            flag = 1
         elem.clear()
+
+    elif event == 'end' and elem.tag == prefix + 'page':
+        if flag == 1:
+            print title
+        flag = 0
 
 #print event, elem
