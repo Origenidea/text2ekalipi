9aa08cc7618bd76c1dc8a6b03f032af75465c37c
Chris McKenzie <kristopolous@yahoo.com>
2015-05-15 20:15:49 -0700

    trying to cleanup the memory

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/python -O
 # -*- coding: utf-8 -*-
-import xml.etree.ElementTree as etree
+import lxml.etree as etree
+import time
+import gc
 import re
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
+gc.enable()
 
 prefix = '{http://www.mediawiki.org/xml/export-0.10/}'
 pr_re = re.compile('{{IPA\|.?([^|\/]*).+lang=en}}', re.M | re.U)
@@ -12,28 +15,84 @@ pr_re = re.compile('{{IPA\|.?([^|\/]*).+lang=en}}', re.M | re.U)
 flag = False 
 text = None
 
-def getpronounce(text):
-    res = pr_re.search(text)
-    if res: 
-        return res.group(1)
-    return res
+tag_title = prefix + 'title'
+tag_text = prefix + 'text'
+tag_ns = prefix + 'ns'
+tag_page = prefix + 'page'
+ix = 0
+start = time.time()
+xmlfile = './ref/enwiktionary.xml'
 
-for event, elem in etree.iterparse('./ref/enwiktionary.xml', events=('start', 'end', 'start-ns', 'end-ns')):
+class TitleTarget(object):
+    def __init__(self):
+        self.text = []
+    def start(self, tag, attrib):
+        self.is_title = True if tag == 'Title' else False
+    def end(self, tag):
+        pass
+    def data(self, data):
+        if self.is_title:
+            self.text.append(data.encode('utf-8'))
+    def close(self):
+        return self.text
+"""
+def fast_iter(context, func, *args, **kwargs):
+    http://lxml.de/parsing.html#modifying-the-tree
+    Based on Liza Daly's fast_iter
+    http://www.ibm.com/developerworks/xml/library/x-hiperfparse/
+    See also http://effbot.org/zone/element-iterparse.htm
+    for event, elem in context:
+        func(elem, *args, **kwargs)
+        # It's safe to call clear() here because no descendants will be
+        # accessed
+        elem.clear()
+        # Also eliminate now-empty references from the root node to elem
+        for ancestor in elem.xpath('ancestor-or-self::*'):
+            while ancestor.getprevious() is not None:
+                del ancestor.getparent()[0]
+    del context
+
+
+def process_element(elem):
+    print elem.xpath( 'description/text( )' )
+
+context = etree.iterparse( xmlfile, tag='' )
+fast_iter(context,process_element)
+"""
+
+for event, elem in etree.iterparse(xmlfile, events=('start', 'end', 'start-ns', 'end-ns')):
+    ix += 1
+    if ix % 100000 == 0:
+        gc.collect()
+        now = time.time()
+        sys.stderr.write(str( ix / (now - start) ) + "\n")
+
     if event == 'start':
-        if elem.tag == prefix + 'title':
+        if elem.tag == tag_title:
             title = elem.text
-        elif elem.tag == prefix + 'text' and flag == 1:
-            text = elem.text
-        elif elem.tag == prefix + 'ns' and elem.text == '0':
+        elif elem.tag == tag_ns and elem.text == '0':
             flag = True
+        elif elem.tag == tag_text and flag:
+            text = elem.text
         elem.clear()
 
-    elif event == 'end' and elem.tag == prefix + 'page':
-        if flag and text and title:
-            res = getpronounce(text)
-            if res != None:
-                print title.decode('utf-8') + ',' + res.decode('utf-8')
-        flag = 0
-        title = None
-        text = None
+    elif event == 'end':
+        if elem.tag == tag_page:
+            if flag and text and title:
+                res = pr_re.search(text)
+                if res: 
+                    print title.decode('utf-8') + ',' + res.group(1).decode('utf-8')
+
+            flag = False
+            title = None
+            text = None
+    elem.clear()
+
+    while elem.getprevious() is not None:
+         del elem.getparent()[0]
+    """
+    for ancestor in elem.xpath('ancestor-or-self::*'):
+        while ancestor.getprevious() is not None:
+            del ancestor.getparent()[0]
+    """
 
