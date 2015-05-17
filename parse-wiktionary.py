34c9d98540d7f5079ed35e1c5d9ffc84d6241a93
Chris McKenzie <kristopolous@yahoo.com>
2015-05-16 18:01:54 -0700

    removing unneeded flags

--- parse-wiktionary.py
+++ parse-wiktionary.py
@@ -13,10 +13,10 @@ sys.setdefaultencoding('utf-8')
 gc.enable()
 
 # This is the pronounciation regex
-pr_re = re.compile('{IPA\s*\|\s*-?.?([^|\/\]]+).+lang=\s*([\-\w]+)', re.M | re.U)
+pr_re = re.compile('{IPA\s*\|\s*-?.?([^|\/\]]+).+lang=\s*([\-\w]+)')
 
 # A number of words have this thing reversed -- "anyone can edit" ...
-pr_re_reversed = re.compile('{IPA\|\s*lang=([\-\w]+)\s*\|\s*[\[\/]?([^}\]\|\/]+)', re.M | re.U)
+pr_re_reversed = re.compile('{IPA\|\s*lang=([\-\w]+)\s*\|\s*[\[\/]?([^}\]\|\/]+)')
 
 # Apparently you can't tell lxml to ignore namespaces so we 
 # construct things here.
