bdb7d29df11d4d463ab0fa1e3404a5dbc00c2711
Chris McKenzie <kristopolous@yahoo.com>
2015-05-16 17:04:08 -0700

    more cleanup

--- test.py
+++ test.py
@@ -16,35 +16,30 @@ gc.enable()
 pr_re = re.compile('{IPA\s*\|\s*.?([^|\/]*).+lang=\s*([\-\w]+)\s*}', re.M | re.U)
 
 # A number of words have this thing reversed -- "anyone can edit" ...
-pr_re_reversed = re.compile('{IPA\|\s*lang=([\-\w]+).{1,2}([^\]|\/]*)[\]\|}]', re.M | re.U)
-
-flag = False 
+pr_re_reversed = re.compile('{IPA\|\s*lang=([\-\w]+).{1,2}([^}\]|\/]*)[\]\|}]', re.M | re.U)
 
 # Apparently you can't tell lxml to ignore namespaces so we 
 # construct things here.
-prefix = '{http://www.mediawiki.org/xml/export-0.10/}'
-tag_title = prefix + 'title'
-tag_text = prefix + 'text'
-tag_ns = prefix + 'ns'
-tag_page = prefix + 'page'
 ix = 0
+total = 0
 start = time.time()
 xmlfile = './ref/enwiktionary.xml'
 
-for event, elem in etree.iterparse(xmlfile, events=('end',), tag=None, attribute_defaults=False, dtd_validation=False, load_dtd=False, no_network=True, remove_blank_text=False, remove_comments=False, remove_pis=False, encoding=None, html=False, recover=None, huge_tree=True):
+for event, elem in etree.iterparse(xmlfile, events=('end',), tag=None, strip_cdata=False, remove_blank_text=True, remove_comments=True, remove_pis=True, encoding='utf-8', huge_tree=True):
 
     ## Get the title of the document (which we may just be ignoring)
-    if elem.tag == tag_title:
+    if elem.tag == '{http://www.mediawiki.org/xml/export-0.10/}title':
         title = elem.text
 
     ## This checks to see if it's an article about a word
-    elif elem.tag == tag_ns and elem.text == '0':
-        flag = True
+    elif elem.tag == '{http://www.mediawiki.org/xml/export-0.10/}ns':
+        total += 1
+        flag = (elem.text == '0')
 
     ## This is the text from the article ... we can process
     ## it here because we have guarantees on its serialized
     ## reading
-    elif elem.tag == tag_text and flag and elem.text:
+    elif elem.tag == '{http://www.mediawiki.org/xml/export-0.10/}text' and flag:
         res = pr_re.search(elem.text)
 
         if res:
@@ -63,18 +58,16 @@ for event, elem in etree.iterparse(xmlfile, events=('end',), tag=None, attribute
                 now = time.time()
                 sys.stderr.write(str(ix) + " " + str( ix / (10 * (now - start)) ) + "\n")
 
-            print title.decode('utf-8') + ',' + (','.join(matches)).decode('utf-8')
+            print title + ',' + (','.join(matches))
         
         #else:
         #    sys.stderr.write( elem.text )
        
 
-    ## Reset all the flags and variables after each page.
-    elif elem.tag == tag_page:
-        flag = False
+    ## Delete the elements to maintain a flat memory --
+    ## Without this the memory usage balloons to the multi-gig
+    ## range
+    if elem.getprevious() is not None:
+       del elem.getparent()[0]
 
-    ## and delete the elements to maintain a flat memory
-    elem.clear()
-
-    while elem.getprevious() is not None:
-         del elem.getparent()[0]
+sys.stderr.write(str(ix) + " pronunciations found out of " + str(total) + " documents in " + str((time.time() - start)) + " seconds.")
