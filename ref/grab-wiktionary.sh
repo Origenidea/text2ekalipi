63f47bc4c165d92888bf7cb7400f5ac4d3d16ac7
Chris McKenzie <kristopolous@yahoo.com>
2015-05-22 20:15:17 -0700

    Cleaning up old wiktionary pull

--- ref/grab-wiktionary.sh
+++ ref/grab-wiktionary.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 wget https://dumps.wikimedia.org/enwiktionary/latest/enwiktionary-latest-pages-articles.xml.bz2
+[ -e enwiktionary-latest-pages-articles.xml ] && rm enwiktionary-latest-pages-articles.xml
 bunzip2 enwiktionary-latest-pages-articles.xml.bz2
 unlink enwiktionary.xml
 ln -s enwiktionary-latest-pages-articles.xml enwiktionary.xml
