fe97cc4d971a0fa418f2dcaca0a7cc9ab2ace1c0
Chris McKenzie <kristopolous@yahoo.com>
2015-08-14 14:06:20 -0700

    adding two ref files

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # text2ekalipi
 
 This is an english text to [ekalipi](www.ekalipi.org) translator using the CMU Sphinx library.
+
+There's also experiments with the [wiktionary ipa](https://dumps.wikimedia.org/enwiktionary) which is a 550MB file when we were dealing with it, which expanded to 4.5GB.  Because of that, it's not included in the repository and should be done on your own.
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/python -O
 # -*- coding: utf-8 -*-
 import xml.etree.ElementTree as etree
+import time
+import gc
 import re
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
+gc.enable()
 
 prefix = '{http://www.mediawiki.org/xml/export-0.10/}'
 pr_re = re.compile('{{IPA\|.?([^|\/]*).+lang=en}}', re.M | re.U)
@@ -12,28 +15,38 @@ pr_re = re.compile('{{IPA\|.?([^|\/]*).+lang=en}}', re.M | re.U)
 flag = False 
 text = None
 
-def getpronounce(text):
-    res = pr_re.search(text)
-    if res: 
-        return res.group(1)
-    return res
+tag_title = prefix + 'title'
+tag_text = prefix + 'text'
+tag_ns = prefix + 'ns'
+tag_page = prefix + 'page'
+ix = 0
+start = time.time()
 
 for event, elem in etree.iterparse('./ref/enwiktionary.xml', events=('start', 'end', 'start-ns', 'end-ns')):
+    ix += 1
+    if ix % 100000 == 0:
+        gc.collect()
+        now = time.time()
+        sys.stderr.write(str( 10000 * ix / (now - start) ) + "\n")
+
     if event == 'start':
-        if elem.tag == prefix + 'title':
+        if elem.tag == tag_title:
             title = elem.text
-        elif elem.tag == prefix + 'text' and flag == 1:
-            text = elem.text
-        elif elem.tag == prefix + 'ns' and elem.text == '0':
+        elif elem.tag == tag_ns and elem.text == '0':
             flag = True
+        elif elem.tag == tag_text and flag:
+            text = elem.text
         elem.clear()
 
-    elif event == 'end' and elem.tag == prefix + 'page':
-        if flag and text and title:
-            res = getpronounce(text)
-            if res != None:
-                print title.decode('utf-8') + ',' + res.decode('utf-8')
-        flag = 0
-        title = None
-        text = None
+    elif event == 'end':
+        if elem.tag == tag_page:
+            if flag and text and title:
+                res = pr_re.search(text)
+                if res: 
+                    print title.decode('utf-8') + ',' + res.group(1).decode('utf-8')
+
+            flag = False
+            title = None
+            text = None
+        elem.clear()
 
