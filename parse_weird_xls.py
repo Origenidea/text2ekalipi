0a63f0faec51ab68eb7c26cc9735dfc151f141fd
Chris McKenzie <kristopolous@yahoo.com>
2015-09-11 13:49:30 -0700

    parsing the weird file now

--- parse_weird_xls.py
+++ parse_weird_xls.py
@@ -2,9 +2,22 @@
 # -*- coding: utf-8 -*-
 import unicodecsv as csv
 import sys
+import gc
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 gc.enable()
 
-csv_file='ref/ipa_kb.csv'
+csv_file = 'ref/ipa_kb.csv'
+csv_handle = open(csv_file, 'rb')
+
+mapper = {}
+first = True
+for row in csv.reader(csv_handle, encoding='utf-8'):
+    if not first:
+        ipa, en, de = row[0], row[1], row[6]
+        mapper[ipa] = {'en': en, 'de': de}
+
+    first = False
+
+print mapper
